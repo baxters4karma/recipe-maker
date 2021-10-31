71fbb8d36cc58c17993ef5655ce6e1e6be3a8542
Naomi Eckhoff <nsuibhne@gmail.com>
2021-10-31 07:16:42 -0500

    basic variable setup

---
+++ assets/edamam.js
@@ -0,0 +1,4 @@
+var edamamIngredient;
+var edamamMealType;
+
+var edamamUrl = "https://api.edamam.com/api/recipes/v2?type=public&" + "q=" + edamamIngredient + "&app_id=ce22d420&app_key=484cfb8615f1c6ea4ed4312af673949f&" + "mealType=" + edamamMealType + "&random=true";
