0eed4f12c32c7e8b72b99314f5dc5869274a1ac1
Naomi Eckhoff <nsuibhne@gmail.com>
2021-11-01 00:24:38 -0500

    added ids to html for click events

--- assets/edamam.js
+++ assets/edamam.js
@@ -1,4 +1,7 @@
-var edamamIngredient;
+
+var edamamIngredientArray = ["chicken", "beef", "pork", "fish", "vegan"];
+var edamamIngredient = edamamIngredientArray[Math.trunc(Math.random() * 5)];
 var edamamMealType;
+
 
 var edamamUrl = "https://api.edamam.com/api/recipes/v2?type=public&" + "q=" + edamamIngredient + "&app_id=ce22d420&app_key=484cfb8615f1c6ea4ed4312af673949f&" + "mealType=" + edamamMealType + "&random=true";
