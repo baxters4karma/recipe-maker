84d688e232ceff3a9d3d2484cd20448dc128bf18
Naomi Eckhoff <nsuibhne@gmail.com>
2021-11-01 08:29:05 -0500

    added in some base click behavior

--- assets/edamam.js
+++ assets/edamam.js
@@ -1,7 +1,24 @@
 
 var edamamIngredientArray = ["chicken", "beef", "pork", "fish", "vegan"];
-var edamamIngredient = edamamIngredientArray[Math.trunc(Math.random() * 5)];
+var edamamIngredient;
 var edamamMealType;
+var edamamUrl;
+var edamamArray = [];
 
+var breakfast = document.getElementById("breakfast");
+var lunch = document.getElementById("lunch");
+var dinner = document.getElementById("dinner");
+var snack = document.getElementById("snack");
 
-var edamamUrl = "https://api.edamam.com/api/recipes/v2?type=public&" + "q=" + edamamIngredient + "&app_id=ce22d420&app_key=484cfb8615f1c6ea4ed4312af673949f&" + "mealType=" + edamamMealType + "&random=true";
+function mealTypeAndFetch(mealSelected) {
+  edamamIngredient = edamamIngredientArray[Math.trunc(Math.random() * 5)];
+  edamamMealType = mealSelected;
+  edamamUrl = "https://api.edamam.com/api/recipes/v2?type=public&q=" + edamamIngredient + "&app_id=ce22d420&app_key=484cfb8615f1c6ea4ed4312af673949f&" + "mealType=" + edamamMealType + "&random=true";
+
+  document.getElementById("meal-dropdown-text").textContent = mealSelected;
+};
+
+breakfast.addEventListener('click', mealTypeAndFetch("breakfast"));
+lunch.addEventListener('click', mealTypeAndFetch("lunch"));
+dinner.addEventListener('click', mealTypeAndFetch("dinner"));
+snack.addEventListener('click', mealTypeAndFetch("snack"));
